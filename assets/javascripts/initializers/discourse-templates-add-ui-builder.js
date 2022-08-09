402c09043dd5d61b8c1e8c29df45dd88a59e1b42
Sérgio Saquetim <saquetim@discourse.org>
2022-08-09 00:15:47 -0300

    Added tests and minor adjustments

--- assets/javascripts/initializers/discourse-templates-add-ui-builder.js
+++ assets/javascripts/initializers/discourse-templates-add-ui-builder.js
@@ -13,66 +13,89 @@ export default {
       currentUser?.can_use_templates
     ) {
       withPluginApi("0.5", (api) => {
-        api.modifyClass("controller:composer", {
-          pluginId: "discourse-templates",
-          actions: {
-            showTemplatesButton() {
-              if (this.site.mobileView) {
-                showModal("discourse-templates-modal");
-              } else {
-                this.appEvents.trigger("composer:show-preview");
-                this.appEvents.trigger("discourse-templates:show");
-              }
-            },
-          },
-        });
-
-        api.addToolbarPopupMenuOptionsCallback(() => {
-          return {
-            id: "discourse_templates_button",
-            icon: "far-clipboard",
-            action: "showTemplatesButton",
-            label: "templates.insert_template",
-          };
-        });
-
-        api.addKeyboardShortcut(
-          "meta+shift+i",
-          () => {
-            const appEvents = container.lookup("service:app-events");
-
-            const activeElement = document.activeElement;
-
-            const composerModel = container.lookup("controller:composer").model;
-            const composerElement = document.querySelector(".d-editor");
-
-            if (composerModel && composerElement?.contains(activeElement)) {
-              appEvents.trigger("composer:show-preview");
-              appEvents.trigger("composer-messages:close");
-              appEvents.trigger("discourse-templates:show");
-              return;
-            }
-
-            if (activeElement?.nodeName === "TEXTAREA") {
-              const modal = document.querySelector(".d-modal");
-
-              if (modal?.contains(activeElement)) {
-                appEvents.trigger("discourse-templates:hijack-modal", {
-                  textarea: activeElement,
-                });
-              } else {
-                const modalController = container.lookup(
-                  "controller:discourse-templates-modal"
-                );
-
-                showModal("discourse-templates-modal");
-                modalController.set("textarea", activeElement);
-              }
-            }
-          },
-          { global: true }
-        );
+        patchComposer(api);
+        addOptionsMenuItem(api);
+        addKeyboardShortcut(api, container);
       });
     }
   },
 };
+
+function patchComposer(api) {
+  api.modifyClass("controller:composer", {
+    pluginId: "discourse-templates",
+    actions: {
+      showTemplatesButton() {
+        if (this.site.mobileView) {
+          showModal("discourse-templates-modal");
+        } else {
+          this.appEvents.trigger("composer:show-preview");
+          this.appEvents.trigger("discourse-templates:show");
+        }
+      },
+    },
+  });
+}
+
+function addOptionsMenuItem(api) {
+  api.addToolbarPopupMenuOptionsCallback(() => {
+    return {
+      id: "discourse_templates_button",
+      icon: "far-clipboard",
+      action: "showTemplatesButton",
+      label: "templates.insert_template",
+    };
+  });
+}
+
+function addKeyboardShortcut(api, container) {
+  const isMac = /Mac|iPod|iPhone|iPad/.test(navigator.platform);
+  const modKey = isMac ? "meta" : "ctrl";
+
+  api.addKeyboardShortcut(
+    `${modKey}+shift+i`,
+    () => {
+      const appEvents = container.lookup("service:app-events");
+
+      const activeElement = document.activeElement;
+
+      const composerModel = container.lookup("controller:composer").model;
+      const composerElement = document.querySelector(".d-editor");
+
+      if (composerModel && composerElement?.contains(activeElement)) {
+        appEvents.trigger("composer:show-preview");
+        appEvents.trigger("composer-messages:close");
+        appEvents.trigger("discourse-templates:show");
+        return;
+      }
+
+      if (activeElement?.nodeName === "TEXTAREA") {
+        const modal = document.querySelector(".d-modal");
+
+        if (modal?.contains(activeElement)) {
+          appEvents.trigger("discourse-templates:hijack-modal", {
+            textarea: activeElement,
+          });
+        } else {
+          const modalController = container.lookup(
+            "controller:discourse-templates-modal"
+          );
+
+          showModal("discourse-templates-modal");
+          modalController.set("textarea", activeElement);
+        }
+      }
+    },
+    {
+      global: true,
+      help: {
+        category: "templates",
+        name: "templates.insert_template",
+        definition: {
+          keys1: [modKey, "shift", "I"],
+          keysDelimiter: "plus",
+        },
+      },
+    }
+  );
+}
