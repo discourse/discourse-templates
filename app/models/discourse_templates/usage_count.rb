3464787ae361303903b661c97c8d39b47c68d959
Gabriel Grubba <gabriel@discourse.org>
2024-10-24 17:12:16 -0300

    DEV: Update plugin annotations

--- app/models/discourse_templates/usage_count.rb
+++ app/models/discourse_templates/usage_count.rb
@@ -9,3 +9,18 @@ module DiscourseTemplates
     validates_presence_of :topic_id
   end
 end
+
+# == Schema Information
+#
+# Table name: discourse_templates_usage_count
+#
+#  id          :bigint           not null, primary key
+#  topic_id    :integer          not null
+#  usage_count :integer          default(0), not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_discourse_templates_usage_count_on_topic_id  (topic_id) UNIQUE
+#
